89570cf6bb4de53e0464b57dd609b21a4b7d2c82
obaida <obaidashurbaji99@gmail.com>
2022-10-23 18:15:38 +0200

    more responsive

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -5,23 +5,21 @@ import woman from '../../assets/possibility.png';
 
 const Possibility = () => {
 	return (
-		<div>
-			<div className="gpt3__possibility" id="possibility">
-				<div className="gpt3__possibility-image">
-					<img src={woman} alt="woman" />
+		<div className="gpt3__possibility" id="possibility">
+			<div className="gpt3__possibility-image">
+				<img src={woman} alt="woman" />
+			</div>
+			<div className="gpt3__possibility-info">
+				<p>Request Early Access to Get Started</p>
+				<div className="gpt3__possibility-info_content">
+					<h2>The possibilities are beyond your imagination</h2>
+					<p>
+						Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all
+						exercise blessing. Indulgence way everything joy alteration boisterous the
+						attachment. Party we years to order allow asked of.
+					</p>
 				</div>
-				<div className="gpt3__possibility-info">
-					<p>Request Early Access to Get Started</p>
-					<div className="gpt3__possibility-info_content">
-						<h2>The possibilities are beyond your imagination</h2>
-						<p>
-							Yet bed any for travelling assistance indulgence unpleasing. Not thoughts
-							all exercise blessing. Indulgence way everything joy alteration boisterous
-							the attachment. Party we years to order allow asked of.
-						</p>
-					</div>
-					<button type="button">Request Early Access to Get Started</button>
-				</div>
+				<button type="button">Request Early Access to Get Started</button>
 			</div>
 		</div>
 	);
