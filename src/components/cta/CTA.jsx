1bdec0d3f013eccab246900819543a9f539edbaa
obaida <obaidashurbaji99@gmail.com>
2022-10-23 19:39:51 +0200

    cta section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import './cta.css';
 
 const CTA = () => {
-	return <div>CTA</div>;
+	return (
+		<div className="cta-container">
+			<div className="content">
+				<div className="register">
+					<p>Request Early Access to Get Started</p>
+					<h1>Register today & start exploring the endless possiblities.</h1>
+				</div>
+				<div className="btn">
+					<button type="button">get started</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CTA;
