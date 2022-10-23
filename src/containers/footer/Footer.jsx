f67da3d48771daa2093dbe9f182b242d2ab57a50
obaida <obaidashurbaji99@gmail.com>
2022-10-23 15:26:35 +0200

    finish responsive footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -16,50 +16,55 @@ const Footer = () => {
 						<h2>gpt-3</h2>
 						<p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
 					</div>
-					<div>
-						<ul>
-							<span>Links</span>
-							<li>
-								<Link to="">Overons</Link>
-							</li>
-							<li>
-								<Link to="">Social Media</Link>
-							</li>
-							<li>
-								<Link to="">Counters</Link>
-							</li>
-							<li>
-								<Link to="">Contact</Link>
-							</li>
-						</ul>
-					</div>
-					<div>
-						<ul>
-							<span>Company</span>
-							<li>
-								<Link to="">Terms & Conditions</Link>
-							</li>
-							<li>
-								<Link to="">Privacy Policy</Link>
-							</li>
-							<li>
-								<Link to="">Contact</Link>
-							</li>
-						</ul>
-					</div>
-					<div>
-						<ul>
-							<span>Get in touch</span>
-							<li>Crechterwoord K12 182 DK Alknjkcb</li>
-							<li>
-								<a href="tel:+085-132567">085-132567</a>
-							</li>
-							<li>
-								<a href="mailto:info@test.net">info@test.net</a>
-							</li>
-						</ul>
+					<div className="navs">
+						<div>
+							<ul>
+								<span>Links</span>
+								<li>
+									<Link to="">Overons</Link>
+								</li>
+								<li>
+									<Link to="">Social Media</Link>
+								</li>
+								<li>
+									<Link to="">Counters</Link>
+								</li>
+								<li>
+									<Link to="">Contact</Link>
+								</li>
+							</ul>
+						</div>
+						<div>
+							<ul>
+								<span>Company</span>
+								<li>
+									<Link to="">Terms & Conditions</Link>
+								</li>
+								<li>
+									<Link to="">Privacy Policy</Link>
+								</li>
+								<li>
+									<Link to="">Contact</Link>
+								</li>
+							</ul>
+						</div>
+						<div>
+							<ul>
+								<span>Get in touch</span>
+								<li>Crechterwoord K12 182 DK Alknjkcb</li>
+								<li>
+									<a href="tel:+085-132567">085-132567</a>
+								</li>
+								<li>
+									<a href="mailto:info@test.net">info@test.net</a>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
+			</div>
+			<div className="footer-3">
+				<p>© 2021 GPT-3. All rights reserved.</p>
 			</div>
 		</div>
 	);
